c15f8c4c0a98f02432ccc34578824f6c3c1fc9f7
Sune Carelse <sune.carelse@takealot.com>
2021-02-03 23:07:00 +0200

    nav bar updated

--- src/Nav.js
+++ src/Nav.js
@@ -18,16 +18,17 @@ function Nav() {
     }, [])
 
   return (
-    <div className="nav nav__black">
+    <div className={`nav ${show && "nav__black"}`}>
         <div className="nav__contents">
         <img 
             className="nav__logo"
             src="https://www.freepnglogos.com/uploads/netflix-logo-history-png-33.png" 
              alt="" 
         />
+
         <img 
             className="nav__avatar"
-            src="https://www.youtube.com/redirect?event=live_chat&redir_token=QUFFLUhqbGU4UXg5WFVrRU9acGJPd3ZDcVptb2d6UlFmQXxBQ3Jtc0ttZjNPbmxJQVM2U2Vqck1ZemRwbGdZOEphZXlTbHlDMDR1LWpaRjlsbk90b0cwVG9kZVFEMnF4Y3ZFb3B2d0o0TW15Z1psNEp5TTlPdzB1c2pSbkl6Zi1YTVNxTHVNMmJLcVBTU2hkVUhUay1xMFVUdw&q=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F1240119990411550720%2FhBEe3tdn_400x400.png"  
+            src="https://seeklogo.net/wp-content/uploads/2012/11/liverbir-vector-logo-400x400.png"  
             alt="" 
         />
       </div>
