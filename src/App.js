a3d158ce68d0ae95bd1f8540399d6d2069da0365
Sune Carelse <sune.carelse@takealot.com>
2021-02-03 20:47:54 +0200

    navbar  renderied in homescreen

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import HomeScreen from './HomeScreen';
 function App() {
   return (
     <div className="app">
-      <h1>Lets build Netflix</h1>
       <HomeScreen />
+
+      {/* Banner */}
+
+      {/* Row */}
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div className="nav">
+      <h1>This is the Nav</h1>
+    </div>
+ )
+}
+
+export default Nav;
