49bb1fa4222b05e4b6f5031391710a6173c422bc
Sune Carelse <sune.carelse@takealot.com>
2021-02-03 20:41:41 +0200

    app created wiyth redux template and cleanup down

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5YH7-bcMidbbqzID-eHjUvH9xjWbrkpY",
+    authDomain: "netflix-build-b4363.firebaseapp.com",
+    projectId: "netflix-build-b4363",
+    storageBucket: "netflix-build-b4363.appspot.com",
+    messagingSenderId: "837760414393",
+    appId: "1:837760414393:web:4d5fec82c584d67e152168"
+  };
