bebe1c08766cf5f66446bf506d54055427aa7c9f
Sune Carelse <sune.carelse@takealot.com>
2021-02-05 01:02:34 +0200

    router dom structure how go to diffrenet screens

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React from 'react';
-import './App.css';
-import HomeScreen from './HomeScreen';
+import React, { useEffect } from "react";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { auth } from "./firebase";
+import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import { login, logout, selectUser } from "./features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <div className="app">
-      <HomeScreen />
-
-      {/* Banner */}
-
-      {/* Row */}
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+      </Router>
     </div>
   );
 }
