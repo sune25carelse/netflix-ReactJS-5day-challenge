4a576ca4ea318303f030b8893ecbe7c30c4e4770
Sune Carelse <sune.carelse@takealot.com>
2021-02-04 20:03:59 +0200

    updated requests

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import './HomeScreen.css';
 import Nav from './Nav';
-import requests from './Requests';
+import requests from './requests';
 import Row from './Row';
 
 function HomeScreen() {
