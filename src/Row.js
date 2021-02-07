d1b9ffcbb9686ac104cd1060bb4a049078153219
Sune Carelse <sune.carelse@takealot.com>
2021-02-07 15:52:02 +0200

    row styling and videos names etc

--- src/Row.js
+++ src/Row.js
@@ -1,33 +1,35 @@
-import axios from './axios';
-import React, { useEffect, useState } from 'react';
-import './Row.css';
+import axios from "./axios";
+import React, { useEffect, useState } from "react";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
   return (
     <div className="row">
       <h2>{title}</h2>
-        <div className="row__posters">
-        {movies.map(movie => (
-            
-        <img 
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             key={movie.id}
-            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-            alt={movie.name}/>
-      ))}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
       </div>
     </div>
   );
